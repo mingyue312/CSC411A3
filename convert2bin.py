1f113895eebd28b2e5a75a321be80405e03da3de
mingyue312 <mingyue312@gmail.com>
2016-11-24 19:46:47 -0500

    added naive image resize split bin data to 7 files

--- convert2bin.py
+++ convert2bin.py
@@ -10,16 +10,19 @@ labels.drop(labels.columns[2], axis=1, inplace=True)
 labels.drop(labels.columns[0], axis=1, inplace=True)
 labels = labels.values
 
-f = open('out.bin', 'wb')
-for i in range(0, len(img_list)):
-    im = Image.open(img_list[i])
-    im = np.array(im)
+for j in range(0, 7):
+    name = 'out'+ str(j) + '.bin'
+    f = open(name,'wb')
+    for i in range(j*1000, (j+1)*1000):
+        im = Image.open(img_list[i])
+        im = np.array(im)
 
-    r = im[:,:,0].flatten()
-    g = im[:,:,1].flatten()
-    b = im[:,:,2].flatten()
-    label = labels[i]
+        r = im[:,:,0].flatten()
+        g = im[:,:,1].flatten()
+        b = im[:,:,2].flatten()
+        label = labels[i]-1
 
-    out = np.array(list(label) + list(r) + list(g) + list(b), np.uint8)
-    #out.tofile("out.bin")
-    f.write(out)
+        out = np.array(list(label) + list(r) + list(g) + list(b), np.uint8)
+        #out.tofile("out.bin")
+        f.write(out)
+    f.close()
